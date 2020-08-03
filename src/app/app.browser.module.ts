e668e902a039591156b40eaa5f8c3072ecd9c4f6
Loic Salou <business.salou@gmail.com>
2020-08-03 15:59:19 +0200

    ajouter le nécessaire pour faire de l'application une PWA

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -3,12 +3,15 @@ import {NgModule} from '@angular/core';
 import {AppModule} from './app.module';
 import {AppComponent} from './app.component';
 import {BrowserModule, BrowserTransferStateModule} from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
             imports: [
               AppModule,
               BrowserModule.withServerTransition({appId: 'angular-starter'}),
-              BrowserTransferStateModule
+              BrowserTransferStateModule,
+              ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
             ],
             bootstrap: [AppComponent]
           })
